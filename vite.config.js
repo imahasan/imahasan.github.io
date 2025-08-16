6b764eedb8fcdaa291ba4bb2b67e74df62706b67
Md Anwarul Hasan <anwar.cse.mis@gmail.com>
2025-08-16 19:14:55 +0200

    Fix: little fix in Vite file, removed an comment and added another

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Personal site on USERNAME.github.io → base '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/'   // personal site (imahasan.github.io)
 })
